7414e649d922686554a7f50b6410d216c848733d
apilatosba <aposdapulos098@outlook.com>
2023-11-23 00:08:32 +0300

    git pusht

---
+++ Not Good Languages/fs.fs
@@ -0,0 +1,5 @@
+for i in [1..100] do
+     if i % 3 = 0 && i % 5 = 0 then printfn "FizzBuzz"
+     elif i % 3 = 0 then printfn "Fizz"
+     elif i % 5 = 0 then printfn "Buzz"
+     else printfn "%i" i
